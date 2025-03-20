d918ebd270a9b40684fdfc7f2dbee8bcb2635bb4
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2025-03-20 17:24:25 -0600

    feat: return api

--- app/Http/Controllers/PartNumberController.php
+++ app/Http/Controllers/PartNumberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Imports\PartNumbersImport;
 use App\Models\YMCOM;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
@@ -14,7 +15,8 @@ class PartNumberController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index() {
+    public function index()
+    {
         //
     }
 
@@ -89,6 +91,18 @@ class PartNumberController extends Controller
         $getStockData = PartNumbersImport::getStockData();
         $getContainersData = PartNumbersImport::getContainersData();
 
+        // if (!is_array($getForecastData)) {
+        //     $getForecastData = [$getForecastData];
+        // }
+
+        // if (!is_array($getStockData)) {
+        //     $getStockData = [$getStockData];
+        // }
+
+        if (!is_array($getContainersData)) {
+            $getContainersData = [$getContainersData];
+        }
+
         $combinedData = [
             'forecast_data' => $getForecastData,
             'stock_data' => $getStockData,
@@ -100,6 +114,29 @@ class PartNumberController extends Controller
 
         Log::alert("Diferencia: {$diff->h} horas, {$diff->i} minutos, {$diff->s} segundos, " . $diff->f * 1000 . " milisegundos.");
 
-        return response()->json($combinedData);
+        $client = new Client();
+
+        try {
+            // Realizar la petición POST a la API de FastAPI (ajusta la URL y el endpoint según corresponda)
+            $response = $client->post('http://10.1.51.200:8000/procesar_json/', [
+                'json' => $combinedData,  // Se envía el JSON directamente
+                'timeout' => 60,  // Tiempo máximo de espera en segundos
+            ]);
+
+            // Convertir la respuesta en JSON
+            $responseData = json_decode($response->getBody(), true);
+
+            // Retornar la respuesta de la API de FastAPI (opcionalmente puedes devolverla a tu frontend)
+            return response()->json([
+                'success' => true,
+                'data' => $responseData
+            ]);
+        } catch (\Exception $e) {
+            // En caso de error, devolver el error
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
